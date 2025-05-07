176fa101ffcacf33a92bbbc2370bbc9f60ffce01
Richard Kanai(李凯男) <richardkanainjeri@gmail.com>
2025-05-07 16:08:46 +0300

    fixed params type

--- app/api/users/getDetails/[id]/route.ts
+++ app/api/users/getDetails/[id]/route.ts
@@ -1,12 +1,11 @@
 import { auth } from "@/lib/auth";
 import prisma from "@/lib/prisma";
 import { Params } from "next/dist/server/request/params";
-import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 
-export async function GET(
-	request: NextRequest,
-	{ params }: { params: Params }
-) {
+export async function GET({ params }:{
+  params: Promise<{ id: string }>
+}) {
 	try {
 		const { id } = await params;
 		const user = await prisma.user.findUnique({
